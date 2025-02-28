ccc8caa5c28cdc6fbe6c8eaaa11a90f005ff00d6
Samer3mad <sameremadwasfy1@gmail.com>
2025-02-28 18:15:50 +0200

    What is ORM and EFCore

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region What is ORM and EFCore
+            ///Object Relational Mapping (ORM) is a technique used in creating a "bridge" between 
+            ///object-oriented programs and, in most cases, relational databases.
+
+            ///When interacting with a database using OOP languages, you'll have to perform different operations 
+            ///like creating, reading, updating, and deleting (CRUD) data from a database. 
+            ///By design, you use SQL for performing these operations in relational databases.*/
+ 
+            ///An ORM tool is software designed to help OOP developers interact with relational databases.
+             ///So instead of creating your own ORM software from scratch, you can make use of these tools.
+             
+            #endregion
         }
     }
 }
